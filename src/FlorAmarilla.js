4631d1ec3669efcfa17c9f0e8089912acee3b40b
ThiagoPharari <thiago.pharari@tecsup.edu.pe>
2025-09-21 22:29:06 -0500

    florsita amarilla

---
+++ src/FlorAmarilla.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './FlorAmarilla.css';
+
+function FlorAmarilla() {
+  return (
+    <div className="flor-container">
+      <div className="flor">
+        <div className="petalo petalo1"></div>
+        <div className="petalo petalo2"></div>
+        <div className="petalo petalo3"></div>
+        <div className="petalo petalo4"></div>
+        <div className="centro"></div>
+        <div className="tallo"></div>
+      </div>
+      <h1 className="mensaje">Te quiero mucho</h1>
+    </div>
+  );
+}
+
+export default FlorAmarilla;
